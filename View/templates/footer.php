4a74d88d74c9a4ba6f79efd4303fe8325004a142
joabe7033 <joabeleal07@gmail.com>
2025-06-14 14:54:12 -0300

    Rename Model to model_temp

--- View/templates/footer.php
+++
@@ -1,9 +0,0 @@
- </main>
-    <footer>
-        <div class="container">
-            <p>&copy; <?= date('Y') ?> Sistema de Receitas - Todos os direitos reservados</p>
-        </div>
-    </footer>
-    <script src="/assets/js/main.js"></script>
-</body>
-</html>
