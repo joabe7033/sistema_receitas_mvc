7fcd504467fbe26629cb061f1227b10248c1b727
joabe7033 <joabeleal07@gmail.com>
2025-06-15 21:28:45 -0300

    Atualiza banco de dados

--- config/receitacadastrologin.sql
+++ config/receitacadastrologin.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 10/06/2025 às 04:34
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Host: localhost
+-- Generation Time: Jun 16, 2025 at 02:27 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,17 +18,14 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `receitacadastrologin`
+-- Database: `receitacadastrologin`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `usuario`
+-- Table structure for table `usuario`
 --
-
-CREATE DATABASE IF NOT EXISTS receitacadastrologin;
-USE receitacadastrologin;
 
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
@@ -36,39 +33,39 @@ CREATE TABLE `usuario` (
   `nomeUsuario` varchar(45) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `cpf` varchar(14) NOT NULL,
-  `data_nascimento` date NOT NULL
+  `data_nascimento` date NOT NULL,
+  `token_recuperacao` varchar(100) DEFAULT NULL,
+  `data_token` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `usuario`
+-- Dumping data for table `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `nome`, `nomeUsuario`, `senha`, `cpf`, `data_nascimento`) VALUES
-(1, 'aaa', 'aa', '$2y$10$cceFCsp0mXUoD'),
-(2, 'aaa', 'aa', '$2y$10$fwuJxjqFFkCqL'),
-(3, 'hh', 'jj', '$2y$10$o7hmAhAbySBmZ');
+INSERT INTO `usuario` (`id`, `nome`, `nomeUsuario`, `senha`, `cpf`, `data_nascimento`, `token_recuperacao`, `data_token`) VALUES
+(31, '3', '3', '$2y$10$Rm/96tPOvYaV2qBv374IPevbOMWBjgud6mob4tgxlN54RTyfIqSUu', '33333333333', '2025-06-15', NULL, NULL),
+(32, '1', '1', '$2y$10$0gwNH1/AzdFABafBQWmkFOuXGvo.Vi31VuvAfYAWaeaXN50g.vYT2', '111.111.111-11', '2025-06-15', NULL, NULL),
+(33, '333', '333', '$2y$10$eEGgav.nvzgBDTruL1jNj.PvfCiBKqgeq0WCeenNyXbJp/1fD2i96', '333.333.333-33', '2025-06-15', NULL, NULL);
 
 --
--- Índices para tabelas despejadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices de tabela `usuario`
+-- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`);
-  ADD COLUMN token_recuperacao VARCHAR(100) DEFAULT NULL,
-  ADD COLUMN data_token DATETIME DEFAULT NULL;
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `usuario`
+-- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
